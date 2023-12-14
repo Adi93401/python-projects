28612b963020f8c160bb032dbe9ab957b0ff5536
Adish Jain <114593154+Adi93401@users.noreply.github.com>
2023-12-14 19:55:33 +0530

    Update chatbot.py

--- Voice Enabled Chatbot With Python and Open Ai/chatbot.py
+++ Voice Enabled Chatbot With Python and Open Ai/chatbot.py
@@ -1,4 +1,6 @@
+from gtts import gTTS
 import openai
+import os
 import speech_recognition as sr
 
 openai.api_key = "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"
@@ -16,6 +18,11 @@ def get_voice_input():
         text = r.recognize_google(audio_data)
     return text
 
+def text_to_speech(text, language='en', output_file='output.mp3'):
+    tts = gTTS(text=text, lang=language, slow=False)
+    tts.save(output_file)
+    os.system("start " + output_file)
+
 def get_response(input):
     response = openai.Completion.create(
   model="text-davinci-003",
@@ -25,12 +32,14 @@ def get_response(input):
 
 if __name__ == "__main__":
     print("Chatbot: Hii user!! How may I help you?")
+    text_to_speech('Hii user!! How may I help you?')
+    while True:
+        user_input = get_voice_input()
+        if user_input == 'exit':
+           break
+        response = get_response(user_input)
+        print(response)
+        text_to_speech(response)
     
-    user_input = get_voice_input()
-    # if user_input == 'exit':
-    #     break
-    response = get_response(user_input)
-    print(response)
-
 ########## ASSIGNMENT ########
 #Get response from chatbot as voice
